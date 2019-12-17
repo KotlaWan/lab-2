23033f026f9e77447e48229d7a5c514aecc641a6
KotlaWan <44785675+KotlaWan@users.noreply.github.com>
2019-12-17 18:37:37 +0300

    Add files via upload

---
+++ Messenger.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Lab2_2
+{
+    public class Messenger
+    {
+        readonly SqlConnection db;
+        int ID;
+
+        public Messenger(string connectionString)
+        {
+            db = new SqlConnection(connectionString);
+            db.Open();
+            ID = db.Query<int>("SELECT MAX(Id) FROM [Chat]").AsList()[0];
+        }
+
+        public async Task SendMessage(Message message)
+        {
+            await db.ExecuteAsync("INSERT INTO [Chat] VALUES (@Name, @Text)", message);
+        }
+
+        public async Task<IEnumerable<Message>> GetMessages()
+        {
+            int newID = db.Query<int>("SELECT MAX(Id) FROM [Chat]").AsList()[0];
+            var q = await db.QueryAsync<Message>("SELECT * FROM [Chat] WHERE [ID] > " + ID + " AND [ID] <= " + newID);
+            ID = newID;
+            return q;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Lab2_2
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("UserName?");
+            string username = Console.ReadLine();
+            //string username = "qwe";
+            var messenger = new Messenger(@"Data Source=KOTLAWAN\SQLEXPRESS;Initial Catalog=ChatBD;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False");
+            
+            Console.Clear();
+            _ = Task.Run(async () =>
+              {
+                  while (true)
+                  {
+                      if (Console.CursorLeft == 0) await ShowMessages(messenger, username);
+                      await Task.Delay(1000);
+                  }
+              });
+            while (true)
+            {
+                string text = Console.ReadLine();
+                if (text != "")
+                {
+                    await messenger.SendMessage(new Message
+                    {
+                        Name = username,
+                        Text = text,
+                    });
+                }
+                int currLine = Console.CursorTop - 1;
+                Console.SetCursorPosition(0, currLine);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, currLine);
+                Console.CursorLeft = 0;
+                await ShowMessages(messenger, username);
+            }
+        }
+
+        static async Task ShowMessages(Messenger messenger, string username)
+        {
+            var messages = await messenger.GetMessages();
+            foreach (var message in messages)
+            {
+                if (message.Name != username) Console.WriteLine(message);
+                else Console.WriteLine(message.Text);
+            }
+        }
+    }
+}
